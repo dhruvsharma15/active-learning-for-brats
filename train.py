50b91dfc60ef1be07f04cff5908248d11353a9af
Dhruv Sharma <dhruv.sharma@kitware.com>
2019-06-03 17:10:35 -0400

    defined GPU to be used

--- train.py
+++ train.py
@@ -1,3 +1,9 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
+ 
+# The GPU id to use, usually either "0" or "1";
+os.environ["CUDA_VISIBLE_DEVICES"]="0";  
+
 from __future__ import print_function
 
 import numpy as np
